d1c4c8176556932b62f71867e03e4d7e5a8264c9
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-07-23 19:01:01 -0400

    Terminando los ultimos arreglos

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ document.querySelector('form').addEventListener('submit', function(e) {
     alert('Formulario enviado exitosamente');
   });
   
+// Mostrar/ocultar el botón de volver al inicio
+window.addEventListener('scroll', function() {
+    const backToTopButton = document.querySelector('.back-to-top');
+    if (window.pageYOffset > 100) {
+      backToTopButton.style.display = 'block';
+    } else {
+      backToTopButton.style.display = 'none';
+    }
+  });
+  
