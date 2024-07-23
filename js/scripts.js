c4d504024ef163eece62a8df2150121a85bf71fa
Juan-Zuniga-Codoceo <zcja.89@gmail.com>
2024-07-23 19:42:17 -0400

    Solucionado

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,3 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulario enviado exitosamente');
-  });
-  
-// Mostrar/ocultar el botón de volver al inicio
 window.addEventListener('scroll', function() {
     const backToTopButton = document.querySelector('.back-to-top');
     if (window.pageYOffset > 100) {
@@ -12,26 +6,43 @@ window.addEventListener('scroll', function() {
       backToTopButton.style.display = 'none';
     }
   });
-  
+
+  // Agregar clase activa a los enlaces del navbar
   document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+      const sections = document.querySelectorAll('section');
+      const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+      const navBarToggler = document.querySelector('.navbar-toggler');
 
-    window.addEventListener('scroll', function() {
-        let current = '';
+      // Función para manejar la activación del enlace del navbar
+      function handleNavLinkActivation() {
+          let current = '';
 
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 60) {
-                current = section.getAttribute('id');
-            }
-        });
+          sections.forEach(section => {
+              const sectionTop = section.offsetTop;
+              if (pageYOffset >= sectionTop - 60) {
+                  current = section.getAttribute('id');
+              }
+          });
 
-        navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (link.getAttribute('href').includes(current)) {
-                link.classList.add('active');
-            }
-        });
-    });
-});
+          navLinks.forEach(link => {
+              link.classList.remove('active');
+              if (link.getAttribute('href').includes(current)) {
+                  link.classList.add('active');
+              }
+          });
+      }
+
+      // Escucha el evento de desplazamiento para activar el enlace del navbar
+      window.addEventListener('scroll', handleNavLinkActivation);
+
+      // Escucha el evento de clic en el toggler para cerrar el menú y activar el enlace
+      navLinks.forEach(link => {
+          link.addEventListener('click', function() {
+              navBarToggler.click();
+              handleNavLinkActivation();
+          });
+      });
+
+      // Llama a la función una vez para activar el enlace correcto al cargar la página
+      handleNavLinkActivation();
+  });
